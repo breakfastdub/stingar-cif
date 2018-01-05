a6a650160959c41f5165411c8f0085356b687914
am577 <breakfastdub@gmail.com>
2018-01-05 14:31:36 -0500

    Added token output, corrected rules copy function, and changed cif version to current beta release.

--- cif-entrypoint.sh
+++ cif-entrypoint.sh
@@ -8,15 +8,18 @@ source /etc/cif.env
 
 # create cif tokens
 if [ ! -f /etc/cif/cif.yml ]; then
-    cif-store --token-create-admin /etc/cif/cif.yml
+    cif-store --token-create-admin --config-path /etc/cif/cif.yml
+    cat /etc/cif/cif.yml
 fi
 
 if [ ! -f /etc/cif/cif-router.yml ]; then
-    cif-store --token-create-hunter /etc/cif/cif-router.yml
+    cif-store --token-create-hunter --config-path /etc/cif/cif-router.yml
+    cat /etc/cif/cif-router.yml
 fi
 
 if [ ! -f /etc/cif/csirtg-smrt.yml ]; then
-    cif-store --token-create-smrt /etc/cif/csirtg-smrt.yml
+    cif-store --token-create-smrt --config-path /etc/cif/csirtg-smrt.yml
+    cat /etc/cif/csirtg-smrt.yml
 fi
 
 chown -R cif:cif /var/lib/cif
